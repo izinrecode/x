ca1c93e47069220bbb9822ae9e11cd6f25d33aed
izinrecode <83387876+izinrecode@users.noreply.github.com>
2021-12-04 21:39:02 +0700

    Create ress.sh

---
+++ ress.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# ==============================
+
+# Banner / Figlet
+read -p "Press Enter For Contitune : ENTER untuk melanjutkan installasi SSH EDUKASI!"
+
+nano /etc/default/dropbear
+/etc/init.d/dropbear restart
+clear
